4fb8fa72fdb1d0b1675178d051715ab1731b8de5
Lucas Brasil <lucasmbrasil2101@gmail.com>
2024-11-07 01:39:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const downloadXLSX = () => {
+  const wb = XLSX.utils.book_new();
+  let numero = document.getElementById('numero')
+  let data = new Date();
+ let dataPt = data.toLocaleDateString()
+  wb.Props = {
+    Title: `Compras dia ${numero.value}`,
+    Subject: 'Compras',
+    Author: 'Lucas',
+    CreatedDate: new Date(),
+  };
+  
+  wb.SheetNames.push('Compras');
+  
+  const dados = [
+   [`${numero.value} pessoas`,''],
+['alcatra', Math.ceil((numero.value/25))],
+['picanha', Math.ceil((numero.value/25))],
+['porco' , Math.ceil((numero.value/16))],
+['drumet', Math.ceil((numero.value/8))],
+['coração', Math.ceil((numero.value/6))],
+['linguiça 5kg', Math.round((numero.value/25))],
+['queijo coalho', Math.ceil((numero.value/25))],
+['',''],
+['água (pack)', Math.floor((numero.value/10))],
+['água c/ gás ',  Math.floor((numero.value/10))],
+['coca',  Math.ceil((numero.value/9))],
+['coca zero',  Math.ceil((numero.value/9))],
+['guaraná', Math.floor((numero.value/10))],
+['guaraná zero', Math.floor((numero.value/10))],
+['mate', Math.ceil((numero.value/6))]
+  ]; 
+  
+  const ws = XLSX.utils.aoa_to_sheet(dados);
+  
+  wb.Sheets['Compras'] = ws;
+   
+  XLSX.writeFile(wb, `Compras ${dataPt}.xlsx`, { bookType: 'xlsx', type: 'bynary'});
+};
+
+document.getElementById('download').addEventListener('click', () => {
+  downloadXLSX();
+  
+})
